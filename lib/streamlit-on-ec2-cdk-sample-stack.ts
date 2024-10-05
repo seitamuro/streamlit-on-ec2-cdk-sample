b42d8fedc51c33cbaa40eb5af7a486ef9a35f9a4
seitamuro <seitamuro0210@gmail.com>
2024-10-05 14:51:14 +0900

    first commit

---
+++ lib/streamlit-on-ec2-cdk-sample-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class StreamlitOnEc2CdkSampleStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'StreamlitOnEc2CdkSampleQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/streamlit-on-ec2-cdk-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StreamlitOnEc2CdkSample from '../lib/streamlit-on-ec2-cdk-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/streamlit-on-ec2-cdk-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StreamlitOnEc2CdkSample.StreamlitOnEc2CdkSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
